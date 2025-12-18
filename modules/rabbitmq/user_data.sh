465f882a17d2ea33b33560f78891de79097d7fc0
omer g <omergldb@gmail.com>
2025-12-18 21:41:31 +0200

    script to aws linux 23

--- modules/rabbitmq/user_data.sh
+++ modules/rabbitmq/user_data.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-# 1. Install RabbitMQ & Erlang
+# 1. Update system
 yum update -y
-yum install -y rabbitmq-server
+
+# 2. Install Erlang (Dependency) and RabbitMQ from Amazon Linux Extras
+# This works for Amazon Linux 2. 
+amazon-linux-extras install epel -y
+amazon-linux-extras install rabbitmq-server -y
+
+# 3. Start and Enable the service
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 
-# 2. Enable Management UI and AWS Peer Discovery Plugin
+# 4. Enable Management UI and Peer Discovery
 rabbitmq-plugins enable rabbitmq_management
 rabbitmq-plugins enable rabbitmq_peer_discovery_aws
 
-# 3. Configure RabbitMQ to find other nodes using AWS Tags
+# 5. Configuration for Clustering
+mkdir -p /etc/rabbitmq
 cat <<EOF > /etc/rabbitmq/rabbitmq.conf
-# Use AWS Peer Discovery
 cluster_formation.peer_discovery_backend = aws
 cluster_formation.aws.region = us-east-1
-# This tells RabbitMQ to look for other nodes in the same ASG
 cluster_formation.aws.use_autoscaling_group = true
-
-# Standard Management Port
 management.tcp.port = 15672
 EOF
 
-# 4. Restart to apply configuration
+# 6. Restart to apply
 systemctl restart rabbitmq-server
